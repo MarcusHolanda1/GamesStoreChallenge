fe730cb1cb206c8e91d96b6a85d9bb67842cf40e
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-10-03 00:18:22 -0300

    feat: fechar e abrir modal

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,5 @@
+import React, {useState} from 'react'
+
 import Header from "../../design/structures/Header";
 import Title from "../../design/components/Title";
 import SelectFilter from "../../design/components/SelectFilter";
@@ -6,24 +8,43 @@ import { Text } from "../../design/components/Text";
 import PrimaryButton from "../../design/components/Button/PrimaryButton";
 import ButtonCart from "../../design/components/Button/ButtonCart";
 import ModalBuy from "../../design/components/ModalBuy";
+import CloseButton from "../../design/components/Button/CloseButton";
 
 import ImageGame from "../../assets/images/callOfDutWWI.png";
 
-import { Container, Image, Star } from "./styles";
+import {
+  Container,
+  Image,
+  Star,
+  ContainerModal,
+  AlignButtonClose,
+} from "./styles";
 
 export default function Home() {
+
+  const [show, setShow] = useState(false);
+   
+  const handleModalClose = (e) => {
+    setShow(false);
+  };
+  
+  const handleModalOpen = () => {
+    setShow(true);
+  };
+  
+
   return (
     <>
-      <ModalBuy>
-        
-      </ModalBuy>
-
-    {/* <ModalBuy>
-       <Card>
+    <div
+        hidden={!show}  
+      >
+       <ContainerModal onClick={handleModalClose}>
+        <Card>
+          <AlignButtonClose onClick={handleModalClose}>
+            <CloseButton></CloseButton>
+            
+          </AlignButtonClose>
           <Image src={ImageGame} alt=""></Image>
-          <Text type="text">
-            <Text type="h5Transparent">PLAYSTATION</Text>
-          </Text>
           <Text type="text">
             <Text type="p">Call of Duty WWII</Text>
           </Text>
@@ -33,21 +54,24 @@ export default function Home() {
             <Star />
             <Star />
             <Star />
-            <Text type="small">(20)</Text>
           </Text>
 
           <Text type="text">
-            <Text type="h4buy">R$ 79,90</Text>
+            <Text type="p">Por apenas</Text>
+          </Text>
+
+          <Text type="text">
+            <Text type="h3BuyModal">R$ 79,90</Text>
           </Text>
 
           <PrimaryButton>
-          <Text type="text">
-            <Text type="buttonText">Adicione ao carrinho</Text>
-          </Text>
-        </PrimaryButton>
-
+            <Text type="text">
+              <Text type="buttonText">Adicione ao carrinho</Text>
+            </Text>
+          </PrimaryButton>
         </Card>
-      </ModalBuy> */}
+      </ContainerModal> 
+      </div>
 
       <Header>
         <ButtonCart>
@@ -64,14 +88,10 @@ export default function Home() {
         </Text>
       </Title>
 
-     
-      
+
       <Container>
-
-       
-
         <Card>
-          <Image src={ImageGame} alt=""></Image>
+          <Image onClick={handleModalOpen} src={ImageGame} alt=""></Image>
           <Text type="text">
             <Text type="h5Transparent">PLAYSTATION</Text>
           </Text>
@@ -92,9 +112,6 @@ export default function Home() {
           </Text>
         </Card>
 
-        
-
-        
       </Container>
     </>
   );
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,32 +1,53 @@
 import styled from "styled-components";
 
-import Theme from "../../design/theme"
+import Theme from "../../design/theme";
 
 import { IoMdStar } from "react-icons/io";
 
 export const Container = styled.section`
   background-color: ${Theme.colors.background};
-    display: flex;
-	flex-wrap: wrap;
-	align-items: space-between;
-	justify-content: center;
-	margin: 0 auto;
-	width: 100%;
- 
+  display: flex;
+  flex-wrap: wrap;
+  align-items: space-between;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  padding-left: ${Theme.spacing.n3};
+  padding-right: ${Theme.spacing.n3};
+  padding-top: ${Theme.spacing.n3};
+`;
 
-    padding-left: ${Theme.spacing.n3};
-    padding-right: ${Theme.spacing.n3};
-    padding-top: ${Theme.spacing.n3};
-`
-
-export const Image = styled.img` ///Enquanto src não estava vindo do component
+export const Image = styled.img`
+  ///Enquanto src não estava vindo do component
   width: 160px;
   height: 230px;
-  
-  padding-bottom:  ${Theme.spacing.n2};
-`
+  padding-bottom: ${Theme.spacing.n2};
+`;
 
 export const Star = styled(IoMdStar)`
-    color: ${Theme.colors.primary};
-    font-size: 18px;
+  color: ${Theme.colors.primary};
+  font-size: 18px;
+`;
+
+///
+
+export const ContainerModal = styled.section`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-width: 100%;
+  min-height: 100%;
+  background: rgba(0, 0, 0, 0.48); ;
+`;
+
+export const StarModal = styled(IoMdStar)`
+  color: ${Theme.colors.primary};
+  font-size: 26px;
+  margin-top: ${Theme.spacing.n1};
+`;
+
+export const AlignButtonClose = styled.div`
+  position: relative;
+  left: 120px;
 `;
