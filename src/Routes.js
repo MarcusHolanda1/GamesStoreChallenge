b3cdb8a76a2e669e93aea8f213bce064e9715aa9
MarcusHolanda1 <guimaraes.marcusvinicius@gmail.com>
2021-10-06 16:09:54 -0300

    feat: rota para tela de compra finalizada

--- src/Routes.js
+++ src/Routes.js
@@ -7,12 +7,14 @@ import {
 
 import Home from './pages/Home/index.jsx'
 import Cart from './pages/Cart/index.jsx';
+import BuyDone from './pages/BuyDone/index.jsx';
 
 const Routes = () => (
     <BRouter>
         <Switch>
             <Route path="/" exact component={ Home } />
             <Route path="/cart" exact component={ Cart } />
+            <Route path="/buydone" exact component={ BuyDone } />
         </Switch>
     </BRouter>
 );
